061cb1e08269e3b6b038ec36679245f54eb87d52
Robert Bittick <robertbittick@gmail.com>
2022-09-23 21:30:02 -0700

    commit tree fbc82b53ee743d193e3b02c8102122f869fce76d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity){
+    if(activity === undefined) {
+        activity = 'roller-skate'
+    }
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(obligation){
+    if (obligation === undefined) {
+        obligation = 'go to the office'
+    }
+    return `This Monday, I will ${obligation}.`
+}
+
+function wrapAdjective(flair) {
+    if (flair === undefined) {
+        flair = '*'
+    }
+    return function(adjective) {
+        if (adjective=== undefined) {
+            adjective = 'special'
+        }
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
